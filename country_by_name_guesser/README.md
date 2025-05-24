6f6012619ab8e36ca194ceb23b67521bf5255323
YuraGavriley <yura.gavriley@gmail.com>
2025-05-24 17:07:05 +0300

    project setup

---
+++ country_by_name_guesser/README.md
@@ -0,0 +1,3 @@
+# Name-to-Country Prediction Service
+
+A RESTful microservice that predicts a person’s likely country of origin based on their name, caches lookup results, and enriches country data with metadata from REST Countries.
---
+++ country_by_name_guesser/country_by_name_guesser/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for country_by_name_guesser project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "country_by_name_guesser.settings")
+
+application = get_asgi_application()
